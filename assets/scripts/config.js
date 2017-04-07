1650af931accf7d92a7e5b8b72bfdf1f811052bb
jim-moody <jim.moody@libertymutual.com>
2017-04-07 10:58:48 -0400

    Updates config to have correct production URL

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,8 +2,10 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://ga-wdi-boston.herokuapp.com'
+    production: 'https://aqueous-atoll-85096.herokuapp.com/'
   }
 }
+
+module.exports = config
 
 module.exports = config
