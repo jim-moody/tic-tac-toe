53fe86d35984518cd86cb65722425a230400b0aa
jim-moody <jim.moody@libertymutual.com>
2017-04-07 16:02:30 -0400

    Adds jasmine tests for game logic
    Adds some tests to make sure the object returned from the
    determine outcome function is correct

--- spec/game-logic.spec.js
+++ spec/game-logic.spec.js
@@ -2,8 +2,29 @@
 
 import { determineOutcome } from '../assets/scripts/games/helpers'
 
-describe('Winning Outcomes', function () {
-  it('should be a winner', function () {
-    expect(determineOutcome).toBe(true)
+describe('Game Logic', () => {
+  it(`returns winner of x and game over`, () => {
+    const board = ['x', 'x', 'x', '', '', '', '', '', '']
+    expect(determineOutcome(board)).toEqual({over: true, winner: 'X'})
+  })
+  it(`returns winner of o and game over`, () => {
+    const board = ['o', 'x', 'x', '', 'o', '', '', '', 'o']
+    expect(determineOutcome(board)).toEqual({over: true, winner: 'O'})
+  })
+  it(`returns winner of x and game over`, () => {
+    const board = ['x', '', '', 'x', '', '', 'x', '', '']
+    expect(determineOutcome(board)).toEqual({over: true, winner: 'X'})
+  })
+  it(`returns correct winner of x and game over`, () => {
+    const board = ['x', 'x', 'o', '', 'o', '', 'x', 'x', 'x']
+    expect(determineOutcome(board)).toEqual({over: true, winner: 'X'})
+  })
+  it(`returns game not over`, () => {
+    const board = ['x', 'o', 'x', '', '', '', '', '', '']
+    expect(determineOutcome(board)).toEqual({over: false})
+  })
+  it(`returns draw and game over `, () => {
+    const board = ['x', 'o', 'x', 'x', 'o', 'o', 'o', 'x', 'x']
+    expect(determineOutcome(board)).toEqual({over: true, winner: 'DRAW'})
   })
 })
